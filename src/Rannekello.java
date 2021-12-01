814c5db4e398ecc15006cab8ce66855590443068
Karri Pajunen <karri.pajunen@student.hamk.fi>
2021-12-01 20:29:55 +0200

    Viimeistely
    muutoksia luokkiin rannekello ja rkello, rkellon poisto ja sen ominaisuuksien ja metodien siirto rannekello luokalle.
    getterin teko.

--- src/Rannekello.java
+++ src/Rannekello.java
@@ -3,23 +3,27 @@ public class Rannekello {
 	
 	public static void main(String[] args) {
 		
-		Rkello olio1 = new Rkello();
+		Rannekello olio1 = new Rannekello();
 		olio1.asetaPvm("01.01.2021");
 		olio1.naytaPvm();
-		Rkello olio2 = new Rkello();
+		Rannekello olio2 = new Rannekello();
 		olio2.asetaAika();
 		olio2.naytaAika();
-		Rkello olio3 = new Rkello();
+		Rannekello olio3 = new Rannekello();
 		olio3.muutaTiedot("Polar","2.1","L","L");
 		
+		urheiluKello UK = new urheiluKello();
+		
+		UK.mittaaSyke(70);
+		System.out.println(UK.getSyke());
+		UK.tiedot();
 		
 		
 		
 	}
-
-} //LOPPU
-
-class Rkello{
+	
+	
+	
 	
 	// scanner ajan ja pvm asetusta varten
 	Scanner in = new Scanner(System.in);
@@ -37,7 +41,7 @@ class Rkello{
 	
 	
 	//muodostin *************************************************************
-	public Rkello() {
+	public Rannekello() {
 		merkki = "Merkki";
 		malli = "Malli";
 		koko = "Koko";
@@ -80,4 +84,69 @@ class Rkello{
 		this.pvm = pvm;
 	}
 }
+
+
+// //LOPPU
+//
+//class Rkello{
+//	
+//	// scanner ajan ja pvm asetusta varten
+//	Scanner in = new Scanner(System.in);
+//	
+//	//muuttujia**********************************************************
+//	String aika = "";
+//	String pvm = "";
+//	
+//	
+//	// luokan parametrit*************************************************
+//	private String merkki;
+//	private String malli;
+//	private String koko;
+//	private String rannenauhanKoko;
+//	
+//	
+//	//muodostin *************************************************************
+//	public Rkello() {
+//		merkki = "Merkki";
+//		malli = "Malli";
+//		koko = "Koko";
+//		rannenauhanKoko = "Rannenauhan koko";
+//	}
+//	
+//	//luokan metodit ************************************************
+//	
+//	// toinen kohdan 6 tapa muuttaa tiedot
+//	protected void muutaTiedot(String merkkiUusi, String malliUusi, String kokoUusi, String rannenauhanKokoUusi) {
+//		merkki = merkkiUusi;
+//		malli = malliUusi;
+//		koko = kokoUusi;
+//		rannenauhanKoko = rannenauhanKokoUusi;
+//		tulostaTiedot();
+//	}
+//	// kohta 5.
+//	private void tulostaTiedot() {
+//		System.out.println("Merkki: " + merkki);
+//		System.out.println("Malli: " + malli);
+//		System.out.println("Koko: " + koko);
+//		System.out.println("Rannenauhan koko: " + rannenauhanKoko);
+//	}
+//	
+//	protected void naytaAika() {
+//		System.out.println("Kello on: " + aika);
+//	}
+//	
+//	protected void naytaPvm() {
+//		System.out.println(pvm);
+//	}
+//	
+//	protected void asetaAika() {
+//		System.out.println("Aseta aika:");
+//		aika = in.nextLine();
+//	}
+//	
+//	protected void asetaPvm(String pvm) {
+//		
+//		this.pvm = pvm;
+//	}
+//}
 
--- src/urheiluKello.java
+++ src/urheiluKello.java
@@ -1,11 +1,22 @@
 
 public class urheiluKello extends Rannekello {
 	
+// ominaisuudet
 	private int syke;
-	
+// metodit
 	protected void mittaaSyke(int syke) {
 		this.syke = syke;
 		System.out.println("Sykkeesi on: " + this.syke);
 	}
+// tarve vain getterille, setterinä toimii metodi mittaasyke()
+	protected int getSyke() {
+		return syke;
+	}
+
+	public void tiedot() {
+		super.muutaTiedot("polar", "urheilu", "L", "L");
+		
+	}
+
 
 }
